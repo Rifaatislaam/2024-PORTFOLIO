139ab1c938244ada40a35c91bc05c9c9fe591ab9
Rifaatislaam <mdrifaatislaam@gmail.com>
2024-07-03 17:21:05 +0600

    Finally Toggle is setup

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
 //TOGGLE NAVBAR ICONS
-let iconMenu = document.querySelector("#icon-menu");
-let navigation = document.querySelector("a");
+let iconMenu = document.querySelector('#icon-menu');
+let navigation = document.querySelector('.navigation');
 
 iconMenu.onclick = () => {
-  iconMenu.classlist.toggle("show data");
-  console.log(a);
+  iconMenu.classList.toggle('bx-x');
 };
 
-//Scroll section
-let sections = document.querySelectorAll("section");
-let navLinks = document.querySelectorAll("header nav a");
 
-window.onscroll = () => {
-  sections.forEach((sec) => {
-    let top = Window.scrollY;
 
-    let offset = sec.offsetTop - 100;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute("id");
 
-    if (top >= offset && top < offset + height) {
-      navLinks.forEach((links) => {
-        links.classList.remove("active");
-        document
-          .querySelector("header nav a[href*=" + id)
-          .classList.add("active");
-      });
-    }
-  });
-};
+
+
+
+
+// //Scroll section
+// let sections = document.querySelectorAll("section");
+// let navLinks = document.querySelectorAll("header nav a");
+
+// window.onscroll = () => {
+//   sections.forEach((sec) => {
+//     let top = Window.scrollY;
+
+//     let offset = sec.offsetTop - 100;
+//     let height = sec.offsetHeight;
+//     let id = sec.getAttribute("id");
+
+//     if (top >= offset && top < offset + height) {
+//       navLinks.forEach((links) => {
+//         links.classList.remove("active");
+//         document
+//           .querySelector("header nav a[href*=" + id)
+//           .classList.add("active");
+//       });
+//     }
+//   });
+// };
